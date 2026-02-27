c6ac45e07ad28c83a23bb40ba7f8a7bd20062a6e
Christopher T. Kenny <ctk44@cornell.edu>
2026-02-27 10:24:44 -0500

    Update floatnote.lua

--- _extensions/apsr/floatnote.lua
+++ _extensions/apsr/floatnote.lua
@@ -3,12 +3,20 @@ return {
     traverse = 'topdown',
     Div = function(div)
         if div.attributes['floatnote'] then
-            local floatnote = div.attributes['floatnote']
+            local floatnote = div.attributes['floatnote']:gsub('%%', '\\%%')
             print(floatnote, '\n')
             local blocks = pandoc.List({})
-            -- floatnote latches to next float, so place before
-            blocks:insert(pandoc.RawInline('latex', '\\floatnote{' .. floatnote .. '}'))
-            blocks:extend(div.content)
+            if div.attributes['tbl-cap'] then
+                -- For tables: keep the div so pandoc creates the table float,
+                -- then place floatnote before so it latches to that float
+                blocks:insert(pandoc.RawBlock('latex', '\\floatnote{' .. floatnote .. '}'))
+                div.attributes['floatnote'] = nil
+                blocks:insert(div)
+            else
+                -- For figures: unwrap div; figure float comes directly from knitr output
+                blocks:insert(pandoc.RawInline('latex', '\\floatnote{' .. floatnote .. '}'))
+                blocks:extend(div.content)
+            end
             return blocks
         end
         return div
